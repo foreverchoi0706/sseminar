8bc0bb7980113975b9fb9e2abf017ac49879cc8a
foreverchoi <foreverchoi0706@gmail.com>
2024-02-27 23:42:47 +0900

    useFetch 개선

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
 
-const useFetch = <T>(
+const useFetch = <T, U = unknown>(
   apiFunction: () => Promise<T>,
-  options?: { onSuccess: (value: T) => void },
+  options?: { onSuccess?: (value: T) => void; onError?: (reason: U) => void },
 ) => {
   const [data, setData] = useState<T | null>(null);
   const [isLoading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    apiFunction().then((value) => {
-      setData(value);
-      options?.onSuccess(value);
-      setLoading(false);
-    });
+    apiFunction()
+      .then((value) => {
+        setData(value);
+        if (options?.onSuccess) options?.onSuccess(value);
+        setLoading(false);
+      })
+      .catch((reason: U) => {
+        if (options?.onError) options.onError(reason);
+      });
   }, []);
 
   return {
